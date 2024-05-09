7a4e594af27ce74d43b671ffe2e8d72897316a3d
Pablo <parusov.pashka@ya.ru>
2024-05-09 18:50:33 +0300

    add an env changes for prod and dev

--- src/app_state/axios/index.js
+++ src/app_state/axios/index.js
@@ -3,10 +3,15 @@ import Cookies from "universal-cookie"
 
 const cookies = new Cookies()
 
+
+const API_DEV_URL = "http://localhost:3001"
+const API_PROD_URL = "https://gastrorace-backend.onrender.com"
+export const STATIC_URL = `${import.meta.env.MODE === "prod" 
+    ? API_PROD_URL 
+    : API_DEV_URL}`
+
 const instance = axios.create({
-    //baseURL: "https://gastrorace-backend-docker.onrender.com",
-    baseURL: "http://localhost:3001",
-    //baseURL: "https://gastrorace-backend.onrender.com"
+    baseURL: STATIC_URL,
 })
 
 instance.interceptors.request.use( config => {
